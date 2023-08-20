58c6b3572efae9aa01810ec47c2b0acad5a2501d
JJablonski02 <kuba2805@icloud.com>
2023-08-20 23:31:54 +0200

    Upload

--- ContactsManager.UI/Program.cs
+++ ContactsManager.UI/Program.cs
@@ -1,21 +1,67 @@
+using ServiceContracts;
+using Services;
+using Microsoft.EntityFrameworkCore;
+using Entities;
+using RepositoryContracts;
+using Repositories;
+using Microsoft.AspNetCore.HttpLogging;
+using Serilog;
+using crudBundle.Filters.ActionFilters;
+using crudBundle;
+using crudBundle.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+
+//logging
+//builder.Host.ConfigureLogging(loggingProvider =>
+//{
+//    loggingProvider.ClearProviders();
+//    loggingProvider.AddConsole();
+//    loggingProvider.AddDebug();
+//    loggingProvider.AddEventLog();
+//});
+
+//Serilog
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider services, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration.ReadFrom.Configuration(context.Configuration).ReadFrom.Services(services); // Reading the configuration
+                                                                                                   // from appsettings.json, reads out
+                                                                                                   // current app's services and make them available to serilog
+});
+
+builder.Services.ConfigureServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Error");
+    app.UseExceptionHandlingMiddleware();
 }
+app.UseSerilogRequestLogging();
+
+app.UseHttpLogging();
+
+//app.Logger.LogDebug("debug-message");
+//app.Logger.LogInformation("information-message");
+//app.Logger.LogCritical("critical-message");
+//app.Logger.LogError("error-message");
+//app.Logger.LogError("warning-message");
+
+if (builder.Environment.IsEnvironment("Test") == false)
+Rotativa.AspNetCore.RotativaConfiguration.Setup("wwwroot", wkhtmltopdfRelativePath: "Rotativa" );
+
 app.UseStaticFiles();
-
 app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
+
+public partial class Program { } //make the auto-generated Program accessible programmatically
