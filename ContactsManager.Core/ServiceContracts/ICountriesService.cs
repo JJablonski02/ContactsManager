48a2736f973f6c38b262bfb28c4ce8968c4e080c
JJablonski02 <kuba2805@icloud.com>
2023-08-20 22:49:34 +0200

    Dodaj pliki projektów.

---
+++ ContactsManager.Core/ServiceContracts/ICountriesService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using ServiceContracts.DTO;
+
+namespace ServiceContracts
+{
+    /// <summary>
+    /// Represents business logic for manipulation Country entity
+    /// </summary>
+    public interface ICountriesService
+    {
+        /// <summary>
+        /// Adds a country object to the list of countries
+        /// </summary>
+        /// <param name="countryAddRequest">Country
+        /// object to add</param>
+        /// <returns>Return the country object after adding it (including newly generated id) </returns>
+        Task<CountryResponse> AddCountry(CountryAddRequest? countryAddRequest);
+
+        /// <summary>
+        /// Returns all countries from the list
+        /// </summary>
+        /// <returns>Returns all countries from the list as List of CountryResponse</returns>
+        Task<List<CountryResponse>> GetAllCountries();
+
+        /// <summary>
+        /// Returns a country object based on the given id
+        /// </summary>
+        /// <param name="countryID">CountryID (guid) to search</param>
+        /// <returns>Matching country as CountryResponse</returns>
+        Task<CountryResponse?> GetCountryByCountryID(Guid? countryID);
+
+        /// <summary>
+        /// Uploads countries from excel file into database
+        /// </summary>
+        /// <param name="fomrFile"></param>
+        /// <returns>Returns the country object after adding it (including newly generated </returns>
+        Task<int> UploadCountriesFromExcelFile(IFormFile formFile);
+    }
+}
---
+++ ContactsManager.Core/Services/CountriesService.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Http;
+using RepositoryContracts;
+using OfficeOpenXml;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using Entities;
+
+namespace Services
+{
+    public class CountriesService : ICountriesService
+    {
+        private readonly ICountriesRepository _countriesRepository;
+
+        //constructor
+        public CountriesService(ICountriesRepository countriesRepository)
+        {
+            _countriesRepository = countriesRepository;
+        }
+        public async Task<CountryResponse> AddCountry(CountryAddRequest? countryAddRequest)
+        {
+
+            //Validation: CountryAddRequest parameter cannot be null
+            if (countryAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(countryAddRequest));
+            }
+            //Validation: CountryName cannot be null
+            if (countryAddRequest.CountryName == null)
+            {
+                throw new ArgumentNullException(nameof(countryAddRequest.CountryName));
+
+            }
+            //Validation: CountryName canot be duplicate
+            if (await _countriesRepository.GetCountryByCountryName(countryAddRequest.CountryName) != null)
+            {
+                throw new ArgumentException("Given country name already exists");
+            }
+
+            //Convert object from CountryAddRequest to Country type
+            Country country = countryAddRequest.ToCountry();
+
+            //generate guid CountryID
+            country.CountryID = Guid.NewGuid();
+
+            //Add country object into _countries
+            await _countriesRepository.AddCountry(country);
+
+            return country.ToCountryResponse();
+
+        }
+
+        public async Task<List<CountryResponse>> GetAllCountries()
+        {
+            List<Country> countries = await _countriesRepository.GetAllCountries();
+            return (await _countriesRepository.GetAllCountries()).Select(country => country.ToCountryResponse()).ToList();
+        }
+
+        public async Task<CountryResponse?> GetCountryByCountryID(Guid? countryID)
+        {
+            if (countryID == null)
+                return null;
+
+            Country? country_response_from_list = await _countriesRepository.GetCountryByCountryID(countryID.Value);
+
+            if (country_response_from_list == null)
+                return null;
+
+            return country_response_from_list.ToCountryResponse();
+        }
+
+        public async Task<int> UploadCountriesFromExcelFile(IFormFile formFile)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            await formFile.CopyToAsync(memoryStream);
+            int countryInserted = 0;
+
+            using (ExcelPackage excelPackage = new ExcelPackage(memoryStream))
+            {
+                ExcelWorksheet workSheet = excelPackage.Workbook.Worksheets["Countries"];
+
+                int rowCount = workSheet.Dimension.Rows;
+
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    string? cellValue = Convert.ToString(workSheet.Cells[row, 1].Value);
+
+                    if(!string.IsNullOrEmpty(cellValue))
+                    {
+                        string? countryName = cellValue;
+
+                        if (_countriesRepository.GetCountryByCountryName(countryName) == null)
+                        {
+                            Country country = new Country() { CountryName = countryName };
+                            await _countriesRepository.AddCountry(country);
+
+                            countryInserted++;
+                        }
+                    }
+                }
+            }
+            return countryInserted;
+        }
+    }
+}
