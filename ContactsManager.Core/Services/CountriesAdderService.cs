fa1357c335dd11f24e564a29b7c62a57d627c69d
JJablonski02 <kuba2805@icloud.com>
2023-08-21 17:48:30 +0200

    Countries Adder Service

---
+++ ContactsManager.Core/Services/CountriesAdderService.cs
@@ -0,0 +1,57 @@
+﻿using Entities;
+using RepositoryContracts;
+using ServiceContracts.DTO;
+using ServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsManager.Core.Services
+{
+
+    public class CountriesAdderService : ICountriesAdderService
+    {
+        private readonly ICountriesRepository _countriesRepository;
+
+        //constructor
+        public CountriesAdderService(ICountriesRepository countriesRepository)
+        {
+            _countriesRepository = countriesRepository;
+        }
+        public async Task<CountryResponse> AddCountry(CountryAddRequest? countryAddRequest)
+        {
+
+            //Validation: CountryAddRequest parameter cannot be null
+            if (countryAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(countryAddRequest));
+            }
+            //Validation: CountryName cannot be null
+            if (countryAddRequest.CountryName == null)
+            {
+                throw new ArgumentNullException(nameof(countryAddRequest.CountryName));
+
+            }
+            //Validation: CountryName canot be duplicate
+            if (await _countriesRepository.GetCountryByCountryName(countryAddRequest.CountryName) != null)
+            {
+                throw new ArgumentException("Given country name already exists");
+            }
+
+            //Convert object from CountryAddRequest to Country type
+            Country country = countryAddRequest.ToCountry();
+
+            //generate guid CountryID
+            country.CountryID = Guid.NewGuid();
+
+            //Add country object into _countries
+            await _countriesRepository.AddCountry(country);
+
+            return country.ToCountryResponse();
+
+        }
+
+    }
+}
